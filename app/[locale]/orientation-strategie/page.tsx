bb3d793536a34476543cb38e945f659c24e861c4
nour jouini <nour.edaher.jouini@gmail.com>
2026-04-29 13:54:48 +0100

    some changes

--- app/[locale]/orientation-strategie/page.tsx
+++ app/[locale]/orientation-strategie/page.tsx
@@ -1,5 +1,6 @@
 import { getDictionary, type Locale } from "../../../dictionaries";
 import SpotBulleMarketingPage from "../../../components/spotbulle/SpotBulleMarketingPage";
+import LandingRevealTitle from "../../../components/spotbulle/LandingRevealTitle";
 
 export default async function OrientationStrategiePage({
   params,
@@ -18,6 +19,28 @@ export default async function OrientationStrategiePage({
       content={content}
       bg2FullWidthRepeat
       preBookingVideoSrc="/videos/section4.mp4"
+      preBookingMobileSection={
+        <div className="pointer-events-auto relative h-[614px] w-full overflow-hidden">
+          <div
+            className="pointer-events-none absolute inset-0 bg-no-repeat"
+            style={{
+              backgroundImage: "url('/images/backgrounds/bg3.png')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+            aria-hidden
+          />
+          <div className="relative h-full w-full">
+            <div className="flex h-full w-full items-center justify-center">
+              <LandingRevealTitle
+                line1={dict.landingReveal.line1}
+                line2={dict.landingReveal.line2}
+                line3={dict.landingReveal.line3}
+              />
+            </div>
+          </div>
+        </div>
+      }
     />
   );
 }
